a68794ee49c95185b3f912b36f039ab0a4c9e9a3
Lucas Lins Bonifácio <94804223+LucasLinsDev@users.noreply.github.com>
2023-07-19 01:58:49 -0300

    Adicionar index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+      
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>lins</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body><section class="section-9adfc835-bd85-4517-b9ce-3aab33623104"><div class="box-25cc9a38-e226-43fd-a85c-fc3804111f8c"><div class="box-4c2af4a0-731b-4dc0-8750-649629bddc94"></div>
+<div class="box-3e682509-755b-4764-b10f-b51924a67ffd"></div>
+</div>
+<div class="box-9e422cce-e2a2-40b0-8b8c-5d483463d103"></div>
+</section>
+
+</body>
+</html>
